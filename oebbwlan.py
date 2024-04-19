8a2000655000ca6cd1f57807f11d83b41fa3c151
Jan Schlüter <jan.schlueter@jku.at>
2024-04-19 10:19:29 +0200

    Fix invalid return statement

--- oebbwlan.py
+++ oebbwlan.py
@@ -4,11 +4,12 @@
 
 import requests
 import bs4
+import sys
 
 sess = requests.Session()
 portal_html = sess.get("http://detectportal.firefox.com").text
 if portal_html.startswith("success"):
-    return
+    sys.exit(0)
 
 tree = bs4.BeautifulSoup(portal_html, "html.parser")
 postdata = {}
